979c8aed83ec9e3938957e7d35c31591f73a36a6
Brian Doan <briankhdo@users.noreply.github.com>
2020-07-23 15:45:38 +0700

    Fix layout on validation and remove unused routes

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,21 +1,7 @@
 class UrlsController < ApplicationController
-  before_action :set_url, only: [:show, :edit, :update, :destroy]
-
-  # GET /urls
-  def index
-  end
-
-  # GET /urls/1
-  def show
-  end
-
   # GET /urls/new
   def new
     @url = Url.new
-  end
-
-  # GET /urls/1/edit
-  def edit
   end
 
   # POST /urls
@@ -25,33 +11,14 @@ class UrlsController < ApplicationController
     if @url.save
       redirect_to root_url, notice: 'Url was successfully created.'
     else
-      render :new
+      render 'home/index'
     end
-  end
-
-  # PATCH/PUT /urls/1
-  def update
-    if @url.update(url_params)
-      redirect_to root_url, notice: 'Url was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /urls/1
-  def destroy
-    @url.destroy
-    redirect_to root_url, notice: 'Url was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_url
-      @url = Url.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def url_params
-      params.require(:url).permit(:source, :key, :clicks_count, :user_id)
+      params.require(:url).permit(:source, :key, :clicks_count)
     end
 end
